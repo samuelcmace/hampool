579d08da25601fbdc14404d1dfd854d2acabead9
Samuel Mace <samuelcmace@gmail.com>
2021-05-13 20:36:22 -0500

    Sort each question in python array.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hampool
 US Ham Radio Question Pool Parsing Tools.
+
+
+## Setup
+1. Download an up-to-date question pool in plaintext from the [NCVEC Website](http://www.ncvec.org/).
+2. Run the script:
+  ```python3 parse.py {inputfile.txt} {outputfile.json}```
---
+++ parse.py
@@ -0,0 +1,30 @@
+import json
+import re
+import sys
+
+# def parseQuestionHead(head):
+#     print(head)
+#
+# def parseQuestionBody(body):
+#     print(body)
+
+try:
+    inputFile = open(sys.argv[1], "r")
+except:
+    print("Error: Unable to read the specified input file.")
+
+questions = re.split("~~", inputFile.read())
+inputFile.seek(0)
+
+print("Question #" + sys.argv[2])
+print(questions[int(sys.argv[2])])
+print("There are a total of " + str(len(questions) + 1) + " in this question pool.")
+
+# try:
+#     outputFile = open(sys.argv[2], "x")
+#     outputFile.write()
+#     outputFile.close()
+# except:
+#     print("Error: Unable to create the specified output file.")
+
+inputFile.close()
